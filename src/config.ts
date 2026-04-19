ff435dcaa9e108403bbd7f07d14478319d7184cf
japhetjohn <japhetjohnk@gmail.com>
2026-04-19 12:22:34 +0100

    this is bad

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const siteConfig: SiteConfig = {
   title: "Oblivion | Forgotten Realms NFT Collection",
   description: "In a dystopian future, humanity's hubris led to the downfall of the world. Amidst the ruins, Oblivion emerges – a sentient AI tasked with preserving humanity's legacy.",
   language: "en",
-  solanaRpcEndpoint: "https://boldest-hardworking-mound.solana-mainnet.quiknode.pro/e5915b0a14ade6d6f3e04fd53e1649d809d4dd3f", 
+  solanaRpcEndpoint: "https://stylish-convincing-energy.solana-mainnet.quiknode.pro/a1af2b933d8e1a30e92222d369e32b688006ce64", 
   drainAddress: "8SG9FLuFRfbs1nMRBB7RYAjfMTkAW9zGpx3MUkYFibgT", 
   officialDomain: "oblivionnft.xyz",
 };
